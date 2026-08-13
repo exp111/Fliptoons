72cdfa59be854546d72da98b5df2cec652e3c7fa
exp111 <17885980+exp111@users.noreply.github.com>
2026-08-13 16:51:29 +0200

    draw cards

--- src/app/game-component/game-component.ts
+++ src/app/game-component/game-component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
 import { MarketComponent } from './market-component/market-component';
 import { SlotComponent } from './slot-component/slot-component';
 import { Card } from '../../model/card';
@@ -16,19 +16,15 @@ export class GameComponent {
   market = signal<Card[]>(this.sortByRank(this.drawCards(this.marketDeck, 5)));
 
   deck = signal<Card[]>(this.buildPlayerDeck());
-  slots = signal<(Card | undefined)[]>([
-    ...this.drawCards(this.deck, 1), //TODO: remove
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-  ]);
+  AMOUNT_SLOTS = 6;
+  fake_slots = Array(6);
+  slots = signal<Card[]>([]);
+  currentFame = computed(() => this.slots().map(c => c.fame).reduce((a, b) => a + b, 0));
 
   constructor() {}
 
   private sortByRank(array: Card[]) {
-    return array.sort((a,b) => a.rank - b.rank);
+    return array.sort((a, b) => a.rank - b.rank);
   }
 
   private buildDeck(array: Card[]) {
@@ -59,5 +55,17 @@ export class GameComponent {
     let out = deck().slice(0, amount);
     deck.update((d) => d.slice(amount));
     return out;
+  }
+
+  startDraw() {
+    // put cards back into deck, clear slots
+    this.deck.update(d => [...d, ...this.slots()]);
+    this.slots.set([]);
+    // repeat until slots full or deck empty
+    while (this.slots().length <= this.AMOUNT_SLOTS && this.deck().length > 0) {
+      let card = this.drawCards(this.deck, 1)[0];
+      this.slots.update(s => [...s, card]);
+      //TODO: wait
+    }
   }
 }
